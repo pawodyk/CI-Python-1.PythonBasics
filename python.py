3db94a1c737c59dc346d2ef61d817c55be8c38fe
Pawel <14218944+pwodyk@users.noreply.github.com>
2019-05-02 22:24:51 +0100

    completed python basics exercises

---
+++ python.py
@@ -0,0 +1,15 @@
+# python basics
+
+def print_message(message):
+    print(message)
+
+print_message("Hello World")
+
+my_list = [1, 2, 3, 4, 5]
+
+for item in my_list:
+    print_message(item)
+
+# Importing Packages
+
+from flask import Flask
